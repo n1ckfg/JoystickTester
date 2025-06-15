c6ca01979935cbb11ff7c0c1cd8d5487aa00e810
Nick Fox-Gieg <nick@fox-gieg.com>
2025-06-15 13:48:53 -0400

    Update JoystickTester.cs

--- Assets/Scripts/JoystickTester.cs
+++ Assets/Scripts/JoystickTester.cs
@@ -2,20 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JoystickTester : MonoBehaviour {
+public class JoystickTester : MonoBehaviour
+{
 
+    public bool checkKeys = true;
+    public bool checkJoysticks = true;
+    public bool checkMouse = true;
     public float updateFrequency = 1f;
-    private IEnumerator Start() {
-        while (true) {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            bool fire1 = Input.GetButtonDown("Fire1");
-            bool fire2 = Input.GetButtonDown("Fire2");
-            bool fire3 = Input.GetButtonDown("Fire3");
-            Debug.Log(horizontalInput + ", " + verticalInput + ", " + fire1 + ", " + fire2 + ", " + fire3);
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            if (checkJoysticks) doCheckJoysticks();
+
+            if (checkKeys) doCheckKeys();
+
+            if (checkMouse) doCheckMouse();
 
             yield return new WaitForSeconds(updateFrequency);
         }
+    }
+
+    void doCheckJoysticks()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        bool fire1 = Input.GetButtonDown("Fire1");
+        bool fire2 = Input.GetButtonDown("Fire2");
+        bool fire3 = Input.GetButtonDown("Fire3");
+
+        Debug.Log(horizontalInput + ", " + verticalInput + ", " + fire1 + ", " + fire2 + ", " + fire3);
+    }
+
+    void doCheckKeys()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(keyCode))
+                {
+                    Debug.Log("Key Pressed: " + keyCode.ToString());
+                }
+            }
+        }
+    }
+
+    void doCheckMouse()
+    {
+        
     }
 
 }
